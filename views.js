7ca20867030c6fdf57e075cae775f26e12911ace
Joel Sommers <jsommers@acm.org>
2014-04-15 21:44:21 -0400

    Clean up index controller/template

--- views.js
+++ views.js
@@ -1,7 +1,4 @@
 exports.index = function (req, res) {
-    console.log("In index with user: " + req.user);
-    console.log("And session id: " + req.session.id);
-
-    res.render('index', {title: "Speed tests", message: "Hello, world", user: req.user});
+    res.render('index', {title: "Speed tests", message: "Hello, world"});
 };
 
